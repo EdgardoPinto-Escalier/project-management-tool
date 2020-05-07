3ff58ff233b56cd0f1c2736e9847400811ecd217
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-05-07 14:16:21 +0200

    Added exceptions to the projects so no duplicates can be added to the database

--- projectmanagementtool/src/main/java/com/edgardopintoescalier/projectmanagementtool/exceptions/ProjectIdExceptionResponse.java
+++ projectmanagementtool/src/main/java/com/edgardopintoescalier/projectmanagementtool/exceptions/ProjectIdExceptionResponse.java
@@ -1,4 +1,17 @@
 package com.edgardopintoescalier.projectmanagementtool.exceptions;
 
 public class ProjectIdExceptionResponse {
+    private String projectIdentifier;
+
+    public ProjectIdExceptionResponse(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
 }
