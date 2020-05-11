9e49b584b0cec22cebe098d877d158e71e0ee055
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-05-11 12:11:47 +0200

    Created the Navbar and Project Item components

--- react-projectmanagementtool/src/components/Dashboard.js
+++ react-projectmanagementtool/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>WELCOME TO THE DASHBOARD</h1>
+        <ProjectItem />
       </div>
     )
   }
