e9953280abdd5ecc6ae9803044612822597d3349
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-05-14 17:07:22 +0200

    Getting the projectId object into the react state

--- react-projectmanagementtool/src/components/Project/UpdateProject.js
+++ react-projectmanagementtool/src/components/Project/UpdateProject.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { getProject } from '../../actions/projectActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
 import { MdSync, MdDateRange } from 'react-icons/md';
 
-export default class UpdateProject extends Component {
+class UpdateProject extends Component {
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.props.getProject(id, this.props.history);
+  }
+
   render() {
     return (
       <div className="register">
@@ -40,3 +50,14 @@ export default class UpdateProject extends Component {
     )
   }
 }
+
+UpdateProject.propTypes = {
+  getProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  project: state.project.project
+});
+
+export default connect(mapStateToProps, {getProject})(UpdateProject);
